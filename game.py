7a17cb704f14a59ebd0f2b20492adeb42989925b
Yaroslav Menshikov <menshikovyar@yandex.ru>
2024-04-04 22:11:29 +0300

    second

--- game.py
+++ game.py
@@ -1,11 +1,10 @@
 import random
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, font as tkFont
 
 colours = ['Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Purple', 'Pink', 'Black', 'White']
 score = 0
 timeleft = 30
-
 
 def next_colour():
     global score, timeleft
@@ -19,82 +18,51 @@ def next_colour():
 
         e.delete(0, tk.END)
         random.shuffle(colours)
-        label.config(fg=colours[1], text=colours[0])
-        score_label.config(text=f"Score: {score}")
-
+        label.config(fg=str(colours[1]), text=str(colours[0]))
+        score_label.config(text="Score: " + str(score))
 
 def countdown():
     global timeleft
     if timeleft > 0:
         timeleft -= 1
-        time_label.config(text=f"Time left: {timeleft}")
+        time_label.config(text="Time left: " + str(timeleft))
         time_label.after(1000, countdown)
     else:
-        # messagebox.showwarning ('Attention', 'Your time is out!!')
-        scoreshow()
+        messagebox.showinfo("Time Over", "Your final score is " + str(score))
 
-
-def record_highest_score():
-    highest_score = load_highest_score()
-    if score > highest_score:
-        with open("highest_score.txt", "w") as file:
-            file.write(str(score))
-
-
-def load_highest_score():
-    try:
-        with open("highest_score.txt", "r") as file:
-            data = file.read()
-            if data:
-                return int(data)
-            else:
-                return 0
-    except FileNotFoundError:
-        return 0
-
-
-def scoreshow():
-    record_highest_score()
-    window2 = tk.Tk()
-    window2.title("HIGH SCORE")
-    window2.geometry("300x200")
-
-    label = tk.Label(window2, text=f"Highest Score: {load_highest_score()}", font=(font, 12))
-
-    label.pack()
-
-    window2.mainloop()
-
-
-def start_game(event):
+def start_game(event=None):
     global timeleft
     if timeleft == 30:
         countdown()
     next_colour()
 
-
 window = tk.Tk()
-font = 'Helvetica'
 window.title("Color Game")
-window.geometry("375x250")
-window.resizable(False, False)
+window.geometry("450x300")
+window.configure(bg="#f0f0f0")
 
-instructions = tk.Label(window, text="Enter the color of the text, not the word!", font=(font, 12))
+app_font = tkFont.Font(family='Helvetica', size=12)
+
+instructions = tk.Label(window, text="Type in the color of the words, and not the word text!",
+                        font=(app_font), bg="#f0f0f0")
 instructions.pack(pady=10)
 
-score_label = tk.Label(window, text="Press Enter to start", font=(font, 12))
+score_label = tk.Label(window, text="Press enter to start", font=(app_font), bg="#f0f0f0")
 score_label.pack()
 
-time_label = tk.Label(window, text=f"Time left: {timeleft}", font=(font, 12))
+time_label = tk.Label(window, text="Time left: " + str(timeleft), font=(app_font), bg="#f0f0f0")
 time_label.pack()
 
-label = tk.Label(window, font=(font, 60))
+label = tk.Label(window, font=('Helvetica', 60), bg="#f0f0f0")
 label.pack(pady=20)
 
-e = tk.Entry(window)
-window.bind('<Return>', start_game)
+e = tk.Entry(window, justify='center', width=20, font=(app_font))
 e.pack()
-
 e.focus_set()
+
+start_button = tk.Button(window, text="Start", command=start_game, width=20, bg="#a0a0a0", fg="#ffffff")
+start_button.pack(pady=20)
+
+window.bind('<Return>', start_game)
 
 window.mainloop()
